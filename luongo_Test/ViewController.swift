e002a9724d64e562d4c6f11c74434601e882476e
Luongo, Jonathan C <luonj004@ww-c02l968xfft0.wdw.disney.com>
2016-07-25 15:25:06 -0400

    Initial Commit

---
+++ luongo_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  luongo_Test
+//
+//  Created by Luongo, Jonathan C. on 7/25/16.
+//  Copyright © 2016 Luongo, Jonathan C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
